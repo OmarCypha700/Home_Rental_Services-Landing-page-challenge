0d84155d0b045cc4ccf1d9153a9f90a711058d93
OmarCypha700 <98944012+OmarCypha700@users.noreply.github.com>
2023-01-24 08:12:12 -0800

    update
    There's an issue with the js and a white space at the side. Will fix it in next upload.

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # Home_Rental_Services-Landing page challenge
  A Landing page challenge.
+
+ This is a fully responsive web landing page for a Home rental company.
+
+## Mobile view
+
+
+## Desktop view
+
+
+## My Process
+### Built with
+
+- Semantic HTML5 markup
+- Mobile first design
+- CSS custom properties
+- Basic javasript
+- jQuery
+
+## Deploying
+This project was deployed with
+- [Netlify](https://www.netlify.com/)
+
+# Links
+- [Live site](https://minimumcost-cypha.netlify.app/)
+
+
+## Authors
+### Code by:
+- Github - [@OmarCypha700](https://github.com/OmarCypha700)
+- Frontend Mentor - [@OmarCypha700](https://www.frontendmentor.io/profile/yourusername)
+- Twitter - [@OmarCypha](https://www.twitter.com/OmarCypha)
+- E-mail - omarcypha@gmail.com
+
+### Challenge by:
+- Twitter - [@codingossy](https://twitter.com/codingossy)
--- main.js
+++ main.js
@@ -19,7 +19,7 @@ window.onload =  () => {
 
    window.onscroll = function () {
 
-       if ( window.innerWidth <= 480 && window.pageYOffset > 0 ) {
+       if ( window.innerWidth >= 320 && window.pageYOffset > 0 ) {
            mobileScrollNav.classList.add("dark__nav");
            mobileScrollNav.classList.remove("transparent__nav");
        }
@@ -28,13 +28,38 @@ window.onload =  () => {
            desktopScrollNav.classList.remove("transparent__nav");
        } 
        else{
-           mobileScrollNav.classList.add("transparent__nav");
-           desktopScrollNav.classList.add("transparent__nav");
-           mobileScrollNav.classList.remove("dark__nav");  
-           desktopScrollNav.classList.remove("dark__nav");
+        if (mobileScrollNav) {
+          mobileScrollNav.classList.add("transparent__nav");
+          mobileScrollNav.classList.remove("dark__nav");
+      }
+      if (desktopScrollNav) {
+          desktopScrollNav.classList.add("transparent__nav");
+          desktopScrollNav.classList.remove("dark__nav");
+      }
        }
 
    }; 
+
+  //  window.onscroll = function () {
+  //   const mobileScrollNav = document.getElementById("mobile_header");
+  //   const desktopScrollNav = document.getElementById("main_header");
+
+  //      if ( window.innerWidth <= 480 && window.pageYOffset > 0 ) {
+  //          mobileScrollNav.classList.add("dark__nav");
+  //          mobileScrollNav.classList.remove("transparent__nav");
+  //      }
+  //      else if (window.innerWidth >= 768 && window.pageYOffset > 0) {
+  //          desktopScrollNav.classList.add("dark__nav");
+  //          desktopScrollNav.classList.remove("transparent__nav");
+  //      } 
+  //     //  else{
+  //     //      mobileScrollNav.classList.add("transparent__nav");
+  //     //      desktopScrollNav.classList.add("transparent__nav");
+  //     //      mobileScrollNav.classList.remove("dark__nav");  
+  //     //      desktopScrollNav.classList.remove("dark__nav");
+  //     //  }
+
+  //  }; 
 
 
     //  Images animation on scroll
@@ -67,14 +92,83 @@ window.onload =  () => {
   observer.observe(document.querySelector('.flex_imgs'));
 
 
-}
+// Handle browse images
+
+  const browseImages = document.getElementById('browse-images');
+  const browse = document.getElementById('browse');
+  
+  browse.addEventListener('click',() => {
+    document.getElementById('browse-images').click();
+  })
+  
+  browseImages.addEventListener('change', (e)=>{
+    const inputFile = e.target.files[0]
+    readImage(inputFile);
+  })
+
+// Drag and drop in form.
+
+  const dropArea = document.getElementById("drop-area");
+  const output = document.getElementById('output');
+  
+  // Handle draging over.
+
+  dropArea.addEventListener("dragover", (event) => {
+    event.preventDefault();
+
+    // Styling the drag and drop as a "copy file" operation.  
+    event.dataTransfer.dropEffect = 'copy';  
+
+    // Styling the drag area when image moves over it.
+    dropArea.classList.add("dragenter");
+  });
+
+     // Handle draging away.
+     dropArea.addEventListener("dragleave", () => {
+
+      // reset background of drop area when the image leaves it
+      dropArea.classList.remove("dragenter");
+    });
 
 
+  // Handle drop
+  dropArea.addEventListener("drop", (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+
+    // Get the files that were dropped
+    const file = event.dataTransfer.files[0];
+
+    // Invoke the readImage function to display file dragged
+    readImage(file);
+
+  });
 
 
+  // Handle displaying image
+  function readImage(file) {
 
+    // Check if the file is an image.
+    if(!file.type.startsWith('image/')){
+      alert(`The selected file is not an image,
+      File type: ${file.type},
+      File name: ${file.name}`)
+      return;  
+    }
     
+    // Use FileReader() to display files
+      const reader = new FileReader();
+      reader.addEventListener('load', () => {
 
+        const image = new Image();
+        image.height = 200
+        image.width = 400
+        image.title = file.name
+        image.src = reader.result
+        output.appendChild(image);
+      });
+  
+      reader.readAsDataURL(file);
+  }
 
-
-
+}
